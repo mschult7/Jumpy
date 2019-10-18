b4c5b8e42132861cb98f7638763ecb51586a8dcb
mattschultz1337 <mschult7@binghamton.edu>
2019-10-17 22:24:04 -0400

    overhaul

---
+++ android/src/edu/binghamton/mschult7/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package edu.binghamton.mschult7;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import edu.binghamton.mschult7.JumpGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new JumpGame(), config);
+	}
+}
---
+++ core/src/edu/binghamton/mschult7/JumpGame.java
@@ -0,0 +1,81 @@
+package edu.binghamton.mschult7;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.*;
+import com.sun.org.apache.xpath.internal.operations.Or;
+
+import java.util.logging.Logger;
+
+import sun.rmi.runtime.Log;
+
+public class JumpGame extends ApplicationAdapter {
+
+	OrthographicCamera camera;
+	ShapeRenderer render;
+	Vector3 coord;
+	Boolean Jump;
+
+
+	@Override
+	public void create () {
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		render = new ShapeRenderer();
+		coord = new Vector3((Gdx.graphics.getWidth() /2) +500, Gdx.graphics.getHeight() / 2, 0);
+		Jump = false;
+	}
+
+	@Override
+	public void render () {
+		camera.update();
+		if(Gdx.input.isTouched()){
+			Jump = true;
+		}
+
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		render.begin(ShapeRenderer.ShapeType.Filled);
+		render.setColor(Color.GREEN);
+		float jumpSpeed = 15;
+		float jumpBoost = 0;
+		float jumpTop = (Gdx.graphics.getWidth()/2) + jumpBoost;
+		float base = (Gdx.graphics.getWidth() /2) + 500;
+		if((int)coord.x <=  base && (int)coord.x > jumpTop && Jump){
+			coord.set(coord.x -jumpSpeed, coord.y,0);
+		} else if(Jump && (int)coord.x <= jumpTop){
+			Jump = false;
+		} else if(!Jump && (int)coord.x >= jumpTop-jumpSpeed && (int)coord.x < base){
+			coord.set(coord.x + jumpSpeed, coord.y, 0);
+		} else {
+			coord.set(coord.x, coord.y, 0);
+		}
+		camera.unproject(coord);
+		render.circle(coord.x, coord.y, 64);
+
+		render.end();
+	}
+
+	@Override
+	public void dispose(){
+		render.dispose();
+	}
+
+	public static void hz(long i){
+		try {
+			Thread.sleep(i);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
